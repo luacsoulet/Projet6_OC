4c8b1da5d5b4c60ee756db6fcb17c2a51121a010
MAGALHAES Lucas <lucasmagalhaes45200@gmail.com>
2024-01-19 16:26:26 +0100

    kasa-app main file update
    Mise à jour du fichier de rooting de l'application pour ajouter le header.

--- kasa-app/src/index.jsx
+++ kasa-app/src/index.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import Header from './components/Header'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
