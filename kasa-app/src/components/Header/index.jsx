3abc71edbb5c0160800ecdd45a737ef100c1a441
MAGALHAES Lucas <lucasmagalhaes45200@gmail.com>
2024-01-19 16:09:56 +0100

    Header maj
    Ajout de styled components pour ajouter du css au header. Ajout du logo avec comme lien accueil lors du clique, ainsi que des liens de navigations de Accueil vers page Home et A propos vers la futur page About.

--- kasa-app/src/components/Header/index.jsx
+++ kasa-app/src/components/Header/index.jsx
@@ -1,6 +1,30 @@
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
 import Logo from '../../assets/nav-logo.png'
 
-function Header() {}
+const HomeLogo = styled.img`
+  height: 210px;
+`
+
+const NavContainer = styled.nav`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 720px
+    margin: 45px 0 50px;
+`
+function Header() {
+  return (
+    <NavContainer>
+      <Link to="/">
+        <HomeLogo src={Logo} />
+      </Link>
+      <div>
+        <Link to="/">Accueil</Link>
+        <Link to="/about">A Propos</Link>
+      </div>
+    </NavContainer>
+  )
+}
 
 export default Header
