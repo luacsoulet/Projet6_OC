399dc4be70bcd085625adaf55d6d0da6f6380c5b
MAGALHAES Lucas <lucasmagalhaes45200@gmail.com>
2024-01-20 12:43:41 +0100

    Header update
    Mise à jour des noms des Class dans le fichier header pour une meilleure visibilité.

--- kasa-app/src/components/Header/index.jsx
+++ kasa-app/src/components/Header/index.jsx
@@ -4,15 +4,15 @@ import Logo from '../../assets/nav-logo.svg'
 
 function Header() {
   return (
-    <div className="navContainer">
+    <div className="nav">
       <NavLink to="/">
         <img src={Logo} alt="Logo Kasa" />
       </NavLink>
       <div>
-        <NavLink className="styledNavLink" to="/">
+        <NavLink className="nav__link" to="/">
           Accueil
         </NavLink>
-        <NavLink className="styledNavLink" to="/about">
+        <NavLink className="nav__link" to="/about">
           A Propos
         </NavLink>
       </div>
