7c35e822b52da4c20bab2ca8290e942433c73550
MAGALHAES Lucas <lucasmagalhaes45200@gmail.com>
2024-01-22 19:45:26 +0100

    Home page update
    Initialisation du contenu de base de la page home. Et l'importation du fichier sass correspondant.

--- kasa-app/src/pages/Home/index.jsx
+++ kasa-app/src/pages/Home/index.jsx
@@ -1,7 +1,15 @@
+import CliffImg from '../../assets/home-background.png'
 import '../../styles/_home.sass'
 
 function Home() {
-  return <div>Hello world</div>
+  return (
+    <div className="home">
+      <div className="home__img">
+        <img src={CliffImg} alt="Falaise pour la page d'accueil" />
+        <p>Chez vous, partout et ailleurs</p>
+      </div>
+    </div>
+  )
 }
 
 export default Home
