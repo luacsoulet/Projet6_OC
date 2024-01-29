aa667a7d0d3632bb2b18650828efbe6e6027b492
MAGALHAES Lucas <lucasmagalhaes45200@gmail.com>
2024-01-29 02:09:15 +0100

    Update About page
    Ajout des collapses d'informations das la page à propos pour donner les différentes informations en passant le titre dans la props title et le contenu dans une props content.

--- kasa-app/src/pages/About/index.jsx
+++ kasa-app/src/pages/About/index.jsx
@@ -1,3 +1,4 @@
+import Collapse from '../../components/Collapse'
 import MountainImg from '../../assets/about-background.png'
 import '../../styles/_about.sass'
 
@@ -6,6 +7,24 @@ function About() {
     <div className="about">
       <div className="about__img">
         <img src={MountainImg} alt="Fond de la page a propos" />
+      </div>
+      <div className="about__collapses">
+        <Collapse
+          title="Fiabilité"
+          content="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes."
+        />
+        <Collapse
+          title="Respect"
+          content="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+        />
+        <Collapse
+          title="Service"
+          content="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+        />
+        <Collapse
+          title="Sécurité"
+          content="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
+        />
       </div>
     </div>
   )
