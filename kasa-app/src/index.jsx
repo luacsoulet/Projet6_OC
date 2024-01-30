814d925c9768589f132befd075820e17ee2675b7
MAGALHAES Lucas <lucasmagalhaes45200@gmail.com>
2024-01-30 02:45:29 +0100

    Kasa-app index.jsx update
    Mise à jour du main fichier de l'application kasa pour ajouter le chemin de LodgingContainer.

--- kasa-app/src/index.jsx
+++ kasa-app/src/index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
+import LodgingContainer from './components/LodgingContainer'
 import Error from './pages/Error'
 import Header from './components/Header'
 import Footer from './components/Footer'
@@ -16,6 +17,7 @@ root.render(
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/lodging/:id" element={<LodgingContainer />} />
         <Route path="*" element={<Error />} />
       </Routes>
       <Footer />
