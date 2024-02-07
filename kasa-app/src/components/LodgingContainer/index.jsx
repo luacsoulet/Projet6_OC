7367be6501c9ad58ec72d5868791e2ea8a2428d2
MAGALHAES Lucas <lucasmagalhaes45200@gmail.com>
2024-02-07 14:49:34 +0100

    LodgingContainer update
    Ajout de commentaire.

--- kasa-app/src/components/LodgingContainer/index.jsx
+++ kasa-app/src/components/LodgingContainer/index.jsx
@@ -2,8 +2,8 @@ import { useParams } from 'react-router-dom'
 import Lodging from '../../pages/Lodging'
 
 function LodgingContainer() {
+  /* On récupère l'id dans l'RL pour la donner en props à la page Lodging  */
   const { id } = useParams()
   return <Lodging id={id} />
 }
-
 export default LodgingContainer
