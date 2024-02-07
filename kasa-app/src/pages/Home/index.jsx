75e6a4ea41d5cf9fd01fe02b0a8b5df32b9a01b9
MAGALHAES Lucas <lucasmagalhaes45200@gmail.com>
2024-02-07 14:51:38 +0100

    Home update
    Ajout des commentaires.

--- kasa-app/src/pages/Home/index.jsx
+++ kasa-app/src/pages/Home/index.jsx
@@ -11,8 +11,13 @@ function Home() {
       <Banner cover={CliffImg} content={'Chez vous, partout et ailleurs'} />
       <div className="home__gallery">
         {KasaData.map((element) => (
-          <Link key={`lodging-${element.id}`} to={`/lodging/${element.id}`}>
+          <Link
+            /* Map sur le fichier data pour récupérer les données nécessaires pour le component Card */
+            key={`lodging-${element.id}`}
+            to={`/lodging/${element.id}`}
+          >
             <Card
+              /* partage des données en props au component Card */
               key={`offer-${element.id}}`}
               label={element.title}
               cover={element.cover}
