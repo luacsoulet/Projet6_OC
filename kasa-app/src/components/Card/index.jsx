138363316023a2813212ba9cf33a1ce9e202f99d
MAGALHAES Lucas <lucasmagalhaes45200@gmail.com>
2024-02-07 14:46:42 +0100

    Card update
    Mise à jour du code en ajoutant les commentaires.

--- kasa-app/src/components/Card/index.jsx
+++ kasa-app/src/components/Card/index.jsx
@@ -1,5 +1,6 @@
 import '../../styles/_card.sass'
 
+/* Création du composant Card en récupérant en propos le label qui est le texte associé à l'annonce, l'image de cover et l'id de l'annonce */
 function Card({ label, cover, id }) {
   return (
     <div className="card" id={id}>
