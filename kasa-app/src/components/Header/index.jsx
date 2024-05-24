52229075070141373e22c0344bd31d2780631e7f
Lucas Magalhaes <119440425+luacsoulet@users.noreply.github.com>
2024-05-24 15:31:58 +0200

    Projet6_frontend-header-component update
    Ajout de la fonctionnalité de lors de la sélection d'un lien de navigation une barre apparaît en dessous du titre du lien. Et si l'écran a la largeur de 375px alors les titres des liens devienne tout en majuscules.

--- kasa-app/src/components/Header/index.jsx
+++ kasa-app/src/components/Header/index.jsx
@@ -1,15 +1,23 @@
 import { NavLink } from 'react-router-dom'
 import '../../styles/_header.sass'
 import Logo from '../../assets/nav-logo.svg'
+import { useState } from 'react'
 
 function Header() {
+
+  const [isSelected, setIsSelected] = useState(false);
+
+  const changeSatete = () => {
+    setIsSelected((prevState) => !prevState);
+  };
+
   return (
     <div className="nav">
       <NavLink to="/">
         <img src={Logo} alt="Logo Kasa" />
       </NavLink>
       <div className="nav__links">
-        <NavLink className="nav__link" to="/">
+        <NavLink className={`nav__link ${isSelected? "active" : null}`} to="/" onClick={changeSatete}>
           Accueil
         </NavLink>
         <NavLink className="nav__link" to="/about">
