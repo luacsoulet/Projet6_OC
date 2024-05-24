6ff4885862c6b8a3f7d4dd09909f3dcc8698ed9f
Lucas Magalhaes <119440425+luacsoulet@users.noreply.github.com>
2024-05-24 21:33:33 +0200

    Projet6_frontend-carrousel-component update
    Mise à jour du code pour mettre une condition sur le nombre d'images fournis en props si il y en a qu'une alors les flèches de navigation et l'index des images sinon affichage de tout les éléments.

--- kasa-app/src/components/Carrousel/index.jsx
+++ kasa-app/src/components/Carrousel/index.jsx
@@ -8,48 +8,56 @@ function Carrousel({ pictures }) {
 
   return (
     <div className="carrousel">
-      <div className="carrousel__buttons">
-        <button
-          className="carrousel__buttons-left"
-          /* au clic sur le bouton de gauche, on vérifie si activeImg est 0 si oui alors activeImg sera égal à la longueur de l'array d'images - 1 sinon -1 à activeImg */
-          onClick={() =>
-            setActiveImg(activeImg === 0 ? pictures.length - 1 : activeImg - 1)
-          }
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="48"
-            height="80"
-            viewBox="0 0 48 80"
-            fill="none"
+      {pictures.length !== 1 ? (
+        <>
+          <div className="carrousel__buttons">
+          <button
+            className="carrousel__buttons-left"
+            /* au clic sur le bouton de gauche, on vérifie si activeImg est 0 si oui alors activeImg sera égal à la longueur de l'array d'images - 1 sinon -1 à activeImg */
+            onClick={() =>
+              setActiveImg(activeImg === 0 ? pictures.length - 1 : activeImg - 1)
+            }
           >
-            <path
-              d="M47.04 7.78312L39.92 0.703125L0.359985 40.3031L39.96 79.9031L47.04 72.8231L14.52 40.3031L47.04 7.78312Z"
-              fill="white"
-            />
-          </svg>
-        </button>
-        <button
-          className="carrousel__buttons-right"
-          /* au clic sur le bouton de droite, on vérifie si activeImg est égal à la longueur de l'array d'images - 1 si oui alors activeImg sera égal 0 sinon + 1 à activeImg */
-          onClick={() =>
-            setActiveImg(activeImg === pictures.length - 1 ? 0 : activeImg + 1)
-          }
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="48"
-            height="80"
-            viewBox="0 0 48 80"
-            fill="none"
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="48"
+              height="80"
+              viewBox="0 0 48 80"
+              fill="none"
+            >
+              <path
+                d="M47.04 7.78312L39.92 0.703125L0.359985 40.3031L39.96 79.9031L47.04 72.8231L14.52 40.3031L47.04 7.78312Z"
+                fill="white"
+              />
+            </svg>
+          </button>
+          <button
+            className="carrousel__buttons-right"
+            /* au clic sur le bouton de droite, on vérifie si activeImg est égal à la longueur de l'array d'images - 1 si oui alors activeImg sera égal 0 sinon + 1 à activeImg */
+            onClick={() =>
+              setActiveImg(activeImg === pictures.length - 1 ? 0 : activeImg + 1)
+            }
           >
-            <path
-              d="M0.960022 72.3458L8.04002 79.4258L47.64 39.8258L8.04002 0.22583L0.960022 7.30583L33.48 39.8258L0.960022 72.3458Z"
-              fill="white"
-            />
-          </svg>
-        </button>
-      </div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="48"
+              height="80"
+              viewBox="0 0 48 80"
+              fill="none"
+            >
+              <path
+                d="M0.960022 72.3458L8.04002 79.4258L47.64 39.8258L8.04002 0.22583L0.960022 7.30583L33.48 39.8258L0.960022 72.3458Z"
+                fill="white"
+              />
+            </svg>
+          </button>
+        </div>
+        <div className="carrousel__counter">
+          {/* Le compteur se met à jour en fonction du state activeImg + 1 si il n'est pas de 0 sur la longueur du tableau pictures  */}
+            {`${activeImg === 0 ? 1 : activeImg + 1}/${pictures.length}`}
+        </div>
+      </>
+      ) : ''}
       <img
         className="carrousel__img-active"
         key={`carrousel-img-${activeImg}`}
@@ -57,10 +65,6 @@ function Carrousel({ pictures }) {
         src={pictures[activeImg]}
         alt=""
       />
-      <div
-        /* Le compteur se met à jour en fonction du state activeImg + 1 si il n'est pas de 0 sur la longueur du tableau pictures */
-        className="carrousel__counter"
-      >{`${activeImg === 0 ? 1 : activeImg + 1}/${pictures.length}`}</div>
     </div>
   )
 }
