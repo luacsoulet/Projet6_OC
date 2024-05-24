49f577c2545bacfb289fb14e9e05b08eda57de0d
Lucas Magalhaes <119440425+luacsoulet@users.noreply.github.com>
2024-05-24 15:40:18 +0200

    Projet6_frontend-header update
    Mise à jour du code du header pour que cela soit moins bugger notamment lors de la sélection d'un des liens seulement un titre est souligner.

--- kasa-app/src/components/Header/index.jsx
+++ kasa-app/src/components/Header/index.jsx
@@ -5,11 +5,7 @@ import { useState } from 'react'
 
 function Header() {
 
-  const [isSelected, setIsSelected] = useState(false);
-
-  const changeSatete = () => {
-    setIsSelected((prevState) => !prevState);
-  };
+  const [isSelected, setIsSelected] = useState(null);
 
   return (
     <div className="nav">
@@ -17,10 +13,10 @@ function Header() {
         <img src={Logo} alt="Logo Kasa" />
       </NavLink>
       <div className="nav__links">
-        <NavLink className={`nav__link ${isSelected? "active" : null}`} to="/" onClick={changeSatete}>
+        <NavLink className={`nav__link ${isSelected === 1 ? "active" : ""}`} to="/" onClick={() => setIsSelected(1)}>
           Accueil
         </NavLink>
-        <NavLink className="nav__link" to="/about">
+        <NavLink className={`nav__link ${isSelected === 2 ? "active" : ""}`} to="/about" onClick={() => setIsSelected(2)}>
           A Propos
         </NavLink>
       </div>
